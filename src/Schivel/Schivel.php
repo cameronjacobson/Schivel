f31242fc7be8e18831fb86f3f888c391d3231cf2
Cameron Jacobson <cameron@datashovel.com>
2013-07-24 20:10:21 +0000

    added methods to manipulate underlying db attribute

--- src/Schivel/Schivel.php
+++ src/Schivel/Schivel.php
@@ -14,6 +14,30 @@ class Schivel
 		$this->db = $db;
 	}
 
+	public function setDatabase($database){
+		$this->db->database = $database;
+	}
+
+	public function setScheme($scheme){
+		$this->db->scheme = $scheme;
+	}
+
+	public function setHost($host){
+		$this->db->host = $host;
+	}
+
+	public function setPort($port){
+		$this->db->port = $port;
+	}
+
+	public function setUsername($user){
+		$this->db->user = $user;
+	}
+
+	public function setPassword($pass){
+		$this->db->pass = $pass;
+	}
+
 	public function store($object){
 		return $this->db->store($object);
 	}
